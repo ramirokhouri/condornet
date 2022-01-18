1fe0300178478864d1f1596a41cbb6487748d513
kuori <ramirokhouri@hotmail.com>
2022-01-18 11:27:04 -0300

    First Commit

--- condornet/settings.py
+++ condornet/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=x=uryg#)eecv%b0q0em2m%*#6zzkwoui)2o9hbxucn+0_97n@'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
